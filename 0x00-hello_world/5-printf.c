1dea084693996ecd78d75d7b0b494e40285dde0b
Everest Obot <everestobot1@gmail.com>
2022-06-29 06:46:25 -0700

    script written

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main entry point
+ * 
+ * using printf function to print => with proper grammar, but the outcome is a piece of art, with a newline
+ *
+ * return 0: (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
