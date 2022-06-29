224575f7753cb872a9cb5d9ee3bd98454ec15940
Everest Obot <everestobot1@gmail.com>
2022-06-29 08:26:47 -0700

    corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,15 +3,10 @@
 /**
  * main entry point
  * 
- * using printf function to print => with proper grammar, but the outcome is a piece of art, with a newline
- *
  * return 0: (success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
-
